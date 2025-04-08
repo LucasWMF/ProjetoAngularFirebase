149d516a1d8525e6b6dcb6981348ec7faed3871a
Lucas Wagner de Melo Fogaça <141058064+LucasWMF@users.noreply.github.com>
2025-04-08 17:55:46 +0000

    prova 2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,17 +12,11 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  pokemon:any = {
-    name: null,
-    power: null
-  };
-
   constructor(
-    public crudService: CrudService
    ){ }
   
-  enviar(){
-    this.crudService.insert(this.pokemon, 'pokemons')
+  botao(){
+    console.log('Noticia Aberta!')
   }
   
 }

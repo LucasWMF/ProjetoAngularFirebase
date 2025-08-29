9f53e3c1825f6765ca63f46ce3148fce6c4dc42c
LucasWMF <lucas.melofogac@gmail.com>
2025-08-29 16:28:53 -0300

    COM ERRO

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,7 @@ import { CrudService } from '../services/crud.service';
 import { Storage, getDownloadURL, ref, uploadBytesResumable } from '@angular/fire/storage';
 import { MessageService } from '../services/message.service';
 import { Router } from '@angular/router';
+import { ApiService } from '../sared/api.service';
 
 @Component({
   selector: 'app-home',
@@ -12,25 +13,20 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  clientes: any = [];
+  usuario: any = {
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+  }
 
   constructor(
-    public crudService: CrudService
-  ) {
-    this.getClient();
+    public apiService: ApiService
+  ) { }
+
+  cadastrarUsuario() {
+    this.apiService.post('usuario/registrar-se', this.usuario).subscribe(resp => {
+      console.log(resp);
+    })
   }
-
-  getClient() {
-    fetch('http://127.0.0.1:8000/api/cliente')
-      .then(resp => resp.json())
-      .then(resp => {
-        console.log(resp);
-        this.clientes = resp;
-      });
-  }
-
-  // enviar() {
-  //   this.crudService.insert(this.pokemon, 'pokemons')
-  // }
-
 }
